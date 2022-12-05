94bde1ce7084332019b8dd62728a03fda054c806
Knucklessg1 <knucklessg1@gmail.com>
2022-12-05 01:30:58 -0600

    Checking in some nice UI changes.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script>
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+
 </script>
 
 <svelte:head>
@@ -10,14 +9,7 @@
 
 <section>
 	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to Svelte
+		Svelte
 	</h1>
 
 	<h2>
@@ -31,27 +23,13 @@
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
-		align-items: center;
-		flex: 0.6;
+		flex: 0.9;
+		background-color: #ffffff;
+		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 	}
 
 	h1 {
 		width: 100%;
 	}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -2,7 +2,7 @@
 	export let open = false
 </script>
 
-<aside class="absolute w-full h-full bg-gray-200 border-r-2 shadow-lg" class:open>
+<aside class="absolute w-full h-full me-auto bg-gray-200 border-r-2 shadow-lg" class:open>
 	<nav class="p-12 text-xl">
 	  <a class="block" href="/">Home</a>
 		<a class="block" href="/about">About</a>
@@ -17,6 +17,10 @@
 	}
 
 	.open {
-		left: 0
+		left: -80%;
+		text-align: right;
+		padding-right: 50;
+		background-color: var(--color-bg-white);
+		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 	}
 </style>
